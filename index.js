4bb9c3e7e99f0373f51df223aed69ada25d0c0fc
balazs4 <balazs4web@gmail.com>
2018-03-23 20:58:09 +0100

    refactor: rename values to mathces

--- index.js
+++ index.js
@@ -9,44 +9,43 @@ const [, , x] = process.argv;
   const page = await browser.newPage();
   await page.goto(`http://livescore.com/${x}`);
 
-  const matches = await page.evaluate(
-    () =>
-      [...document.querySelector('div[data-type=container]').children]
-        .filter(x => x.attributes['data-type'])
-        .reduce(
-          (acc, row) => {
-            const type = row.attributes['data-type'].value;
-            if (type === 'stg') {
-              const id = row.attributes['data-stg-id'].value;
-              const date = row.attributes['data-id'].value;
+  const { matches } = await page.evaluate(() =>
+    [...document.querySelector('div[data-type=container]').children]
+      .filter(x => x.attributes['data-type'])
+      .reduce(
+        (acc, row) => {
+          const type = row.attributes['data-type'].value;
+          if (type === 'stg') {
+            const id = row.attributes['data-stg-id'].value;
+            const date = row.attributes['data-id'].value;
 
-              if (acc.meta.id !== id) {
-                const name = row.children[0].children[0].innerText;
-                acc.meta = Object.assign(acc.meta, { id, name, date });
-              }
-              if (acc.meta.date !== date) {
-                acc.meta.date = date;
-              }
+            if (acc.meta.id !== id) {
+              const name = row.children[0].children[0].innerText;
+              acc.meta = Object.assign(acc.meta, { id, name, date });
             }
-            if (type === 'evt') {
-              const [home, away] = [...row.querySelectorAll('.ply.name')].map(
-                x => x.innerText.trim()
-              );
-              const min = row.querySelector('.min').innerText.trim();
-              const score = row.querySelector('.sco').innerText.trim();
-              const match = {
-                home,
-                away,
-                score,
-                min,
-                score
-              };
-              acc.values.push(Object.assign(match, acc.meta));
+            if (acc.meta.date !== date) {
+              acc.meta.date = date;
             }
-            return acc;
-          },
-          { values: [], meta: { id: null, date: null, name: null } }
-        ).values
+          }
+          if (type === 'evt') {
+            const [home, away] = [...row.querySelectorAll('.ply.name')].map(x =>
+              x.innerText.trim()
+            );
+            const min = row.querySelector('.min').innerText.trim();
+            const score = row.querySelector('.sco').innerText.trim();
+            const match = {
+              home,
+              away,
+              score,
+              min,
+              score
+            };
+            acc.matches.push(Object.assign(match, acc.meta));
+          }
+          return acc;
+        },
+        { matches: [], meta: { id: null, date: null, name: null } }
+      )
   );
   console.log(JSON.stringify(matches, null, 2));
   await browser.close();
