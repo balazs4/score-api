7a212dc4fd8cd2b5ee22290ee769e03b15c7ac6b
balazs4 <balazs4web@gmail.com>
2018-04-01 17:22:44 +0200

    very first graphql

--- index.js
+++ index.js
@@ -8,18 +8,62 @@ const fn = filterText
   ? ({ home, away }) => [home, away].some(x => x.includes(filterText))
   : _ => true;
 
-(async () => {
-  const browser = await puppeteer.launch({
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
-  });
+//(async () => {
+//  const browser = await puppeteer.launch({
+//    args: ['--no-sandbox', '--disable-setuid-sandbox']
+//  });
+//
+//  const getAllMatches = setupGetAllMatches(browser);
+//  const getMatchDetails = setupGetMatchDetails(browser);
+//
+//  const matches = await getAllMatches(resource);
+//  const details = await Promise.all(matches.filter(fn).map(getMatchDetails));
+//
+//  await browser.close();
+//  console.log(JSON.stringify(details, null, 2));
+//  return details;
+//})();
+//
+//
+//
+const { microGraphql } = require('apollo-server-micro');
+const { makeExecutableSchema } = require('graphql-tools');
 
-  const getAllMatches = setupGetAllMatches(browser);
-  const getMatchDetails = setupGetMatchDetails(browser);
+const typeDefs = `
+   type Match {
+     home: String
+     away: String
+     href: String
+     min: String
+   }
 
-  const matches = await getAllMatches(resource);
-  const details = await Promise.all(matches.filter(fn).map(getMatchDetails));
+   type Query {
+     matches(resource: String): [Match]
+     match(href: String!): [Match]
+   }
+`;
 
-  await browser.close();
-  console.log(JSON.stringify(details, null, 2));
-  return details;
-})();
+const resolvers = {
+  Query: {
+    matches(resource) {
+      console.log(resource);
+      return puppeteer
+        .launch({
+          args: ['--no-sandbox', '--disable-setuid-sandbox']
+        })
+        .then(setupGetAllMatches)
+        .then(fn => fn(resource || '/soccer/'));
+    },
+    match(href) {
+      return puppeteer
+        .launch({
+          args: ['--no-sandbox', '--disable-setuid-sandbox']
+        })
+        .then(setupGetMatchDetails)
+        .then(fn => fn(href));
+    }
+  }
+};
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+module.exports = microGraphql({ schema });
